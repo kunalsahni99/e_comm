93c29e69c97aeb2be825fb93d8828c3cb01e808e
Kunal <kunal.sahni1999@gmail.com>
2025-04-28 08:20:16 +0530

    first commit

---
+++ api/models.py
@@ -0,0 +1,54 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    pass
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    img = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    @property
+    def in_stock(self):
+        return self.stock > 0
+    
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = 'Pending'
+        CONFIRMED = 'Confirmed'
+        CANCELLED = 'Cancelled'
+        DELIVERED = 'Delivered'
+
+    orderID = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=10, 
+        choices=StatusChoices.choices, 
+        default=StatusChoices.PENDING)
+    
+    products = models.ManyToManyField(Product, through='OrderItem', related_name='orders')
+
+    def __str__(self):
+        return f"Order {self.orderID} - {self.user.username}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    @property
+    def item_subtotal(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity}) in {self.order.orderID}"
+    
