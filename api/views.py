00b9e81f455a63909d23f87ae45e6400bb13f354
rahul38888 <kunal.sahni1999@gmail.com>
2025-05-02 22:01:36 +0530

    Serializers and Response objects

---
+++ api/views.py
@@ -0,0 +1,17 @@
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from api.serializers import ProductSerializer
+from api.models import Product
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
--- e_comm/urls.py
+++ e_comm/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import product_list, product_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', product_list),
+    path('products/<int:pk>', product_detail)
 ]
